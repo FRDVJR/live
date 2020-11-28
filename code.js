3e099e17cc80df19f1709f2171e3247ed6b4f0d4
Felix Del Valle <frdvjr@GmAil.com>
2020-11-27 22:26:16 -0500

    added hella ammount of letter

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 console.log("Hello!  My name is Sean.")
 
-for(var count = 0; count < 50; count++)
+for(var count = 0; count < 5100000000000000000000; count++)
 {
     console.warn("Something is wrong!!!")
 }
